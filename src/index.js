cd65d8d7ff313310e4e47ff91a2cd3eeb8fda0dd
Katherine <katrose@users.noreply.github.com>
2019-10-08 17:48:39 -0600

    Trying to refactor code in to React app
    also deleted extra files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return (
+      <nav class="navbar">
+        <div class="container">
+            <div class="logo"></div>
+              <ul>
+                <li>World</li>
+                <li>Sports</li>
+                <li>Opinion</li>
+                <li><strong>Lifestyle</strong></li>
+                <li>Business</li>
+                <li>Culture</li>
+                <li>Fashion</li>
+                <li>Tech</li>
+             </ul>
+            <span class="icon"><i class="ion-ionic"></i></span>
+        </div>
+    </nav>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
