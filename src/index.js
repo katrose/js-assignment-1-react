cd6b09eeefbde80499ffb236343f46c6a83a3c2c
Katherine <katrose@users.noreply.github.com>
2019-10-08 21:01:49 -0600

    added Footer component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ class App extends Component {
       <Fragment>
         <NavBar />
         <TileContainer data={DATA}/>
+        <Footer />
       </Fragment>
     )
   }
@@ -93,6 +94,22 @@ class Tile extends Component {
         <h2 className="title">{title}</h2>
         <div className="buy-button">{price}</div>
       </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return(
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+        </div>
+    </footer>
     )
   }
 }
