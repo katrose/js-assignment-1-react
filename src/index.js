1010c20d6fbbf2e5c9f77fd511a5da77e1a3da71
Katherine <katrose@users.noreply.github.com>
2019-10-15 12:25:18 -0600

    removed data array from index.js to put in a different file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import NavBar from './components/NavBar.js'
 import TileContainer from './components/TileContainer.js'
 import Footer from './components/Footer.js'
 
+import DATA from './components/data.js'
+
 import './index.css';
 
 class App extends Component {
@@ -19,51 +21,5 @@ class App extends Component {
     )
   }
 }
-
-const green = 'rgba(118,174, 170, 1)'
-const black = 'rgba(23,35, 50, 1)'
-const brown = 'rgba(205,164, 133, 1)'
-const greenblue = 'rgba(62,171, 201, 1)'
-const pink = 'rgba(254,156, 161, 1)'
-const blue = 'rgba(98,189, 254, 1)'
-
-const DATA = [
-    {
-        topic: 'Food',
-        title: 'Wake Up and Smell the Coffee',
-        price: '$0.90',
-        color: green
-    },
-    {
-        topic: 'Architecture',
-        title: 'The Brand New NASA Office',
-        price: '$0.19',
-        color: black
-    },
-    {
-        topic: 'Travel',
-        title: 'Experience the Saharan Sands',
-        price: '$2.29',
-        color: brown
-    },
-    {
-        topic: 'Interior',
-        title: '9 Air-Cleaning Plants Your Home Needs',
-        price: '$0.09',
-        color: greenblue
-    },
-    {
-        topic: 'Food',
-        title: 'One Month Sugar Detox',
-        price: '$0.99',
-        color: pink
-    },
-    {
-        topic: 'Photography',
-        title: 'Shooting Minimal Instagram Photos',
-        price: '$0.29',
-        color: blue
-    }
-]
 
 ReactDOM.render(<App />, document.getElementById('root'));
